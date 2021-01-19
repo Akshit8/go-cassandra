17e8b298076a06411ebe6a09b69770c3f43818b4
Akshit Sadana <akshitsadana@gmail.com>
2021-01-19 17:43:03 +0530

    config mgmt setup2

--- config/config.go
+++ config/config.go
@@ -15,14 +15,28 @@ import (
 // Config stores all configuration of the application.
 // The values are ready by viper from a config file or environment variable
 type Config struct {
+	AppPort       string `mapstructure:"APP_PORT"`
 	CassandraHost string `mapstructure:"CASSANDRA_HOST"`
 	CassandraPort int    `mapstructure:"CASSANDRA_PORT"`
 }
 
 // LoadConfigFromEnv loads env variables to config object
 func LoadConfigFromEnv() (config Config, err error) {
+	viper.SetConfigName("config")
+	viper.SetConfigType("yml")
+	viper.AddConfigPath("./config")
+
+	if err = viper.ReadInConfig(); err != nil {
+		return
+	}
+
+	// Enable VIPER to read Environment Variables
 	viper.AutomaticEnv()
-	config.CassandraHost = viper.GetString("CASSANDRA_HOST")
-	config.CassandraPort = viper.GetInt("CASSANDRA_PORT")
+
+	err = viper.Unmarshal(&config)
+	if err != nil {
+		return
+	}
+	
 	return
 }
